76b0378cf556e9d89b1d2e8a469a0ce1ca772d08
yateesh013 <yateeshec013@gmail.com>
2023-10-30 20:13:03 +0530

    COMMENTS ARE UPDATED TO FILE

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
+# FUNCTION FOR DEFINING THE nth MAX NUMBER IN ALIST
 def max_of_number(my_list,n):
     m=[]
-
+    #DEFINIG EMPTY LIST
     if(len(set(my_list))<n):
         return ValueError("error")
-    
+    #CHECKING FOR THE LENGTH ERROR 
+
+    #GETTING THE MAXIMUM NUMBER IN LIST.
     for i in range(n):
         lar=0
         for j in range(len(my_list)):
+            #CHECKING THE NUMBER IS ALREADY PRESENT OR NOT
             if my_list[j]>lar and my_list[j] not in m :
                 lar=my_list[j]
         m.append(lar)            
               
     return lar            
-                            
+    #RETURNING THE NUMBER.                        
